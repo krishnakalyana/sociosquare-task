30ca9a2778a50410fe2b2b43c77cacf3f69703cc
krishnakalyana <krishnakalyanax@gmail.com>
2024-04-28 10:58:07 +0530

    updated register form

---
+++ app/(public)/register/_components/RegisterForm.jsx
@@ -0,0 +1,123 @@
+'use client'
+import {
+  Alert,
+  Button,
+  Divider,
+  Grid,
+  Snackbar,
+  TextField,
+  Typography
+} from '@mui/material'
+import { pages } from '@/utils/constants'
+import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/navigation'
+import { useCreateUserMutation } from '@/libs/api/users'
+import { useState } from 'react'
+export default function RegisterForm() {
+  const { register, handleSubmit } = useForm()
+  const router = useRouter()
+  const [error, setError] = useState(false)
+  const [createUser, { isLoading }] = useCreateUserMutation()
+
+  const handleCreateUser = async data => {
+    try {
+      const response = await createUser(data).unwrap()
+      console.log('New user created:', response)
+      sessionStorage.setItem('isLoggedIn', true)
+      router.push(pages.Dashboard)
+      /*
+        since the api does not give anything back assuming the user creating is success
+        in this case redirecting the user to dashboard page
+        and saving a key in session to keep track of login status in this case just
+        storing a true or false state
+        otherwise might use JWT token for authentication.
+        */
+    } catch (error) {
+      setError(true)
+      console.error('Failed to create User:', error)
+    }
+  }
+  const onSubmit = data => {
+    handleCreateUser(data)
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container rowSpacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              size='small'
+              {...register('fullname')}
+              label='Full Name'
+              variant='outlined'
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              size='small'
+              {...register('username')}
+              label='Username'
+              variant='outlined'
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              size='small'
+              {...register('password')}
+              label='Password'
+              type='password'
+              variant='outlined'
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              type='submit'
+              variant='contained'
+              color='primary'
+              fullWidth
+              disabled={isLoading}
+            >
+              {isLoading ? 'Loading' : 'Register'}
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+      <Divider flexItem>
+        <Typography align='center' variant='overline'>
+          OR
+        </Typography>
+      </Divider>
+      <Button
+        type='submit'
+        variant='outlined'
+        color='primary'
+        fullWidth
+        onClick={() => {
+          router.push(pages.LOGIN)
+        }}
+      >
+        Login
+      </Button>
+      <Snackbar
+        open={error}
+        autoHideDuration={5000}
+        onClose={() => setError(false)}
+      >
+        <Alert
+          onClose={() => setError(false)}
+          severity='error'
+          variant='filled'
+          sx={{ width: '100%' }}
+        >
+          Something went wrong please try again.
+        </Alert>
+      </Snackbar>
+    </>
+  )
+}
--- app/(public)/register/page.jsx
+++ app/(public)/register/page.jsx
@@ -1,131 +1,28 @@
-'use client'
-import {
-  Alert,
-  Button,
-  Divider,
-  Grid,
-  Snackbar,
-  TextField,
-  Typography
-} from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import CardContainer from '../../_components/CardContainer'
-import { pages } from '@/utils/constants'
-import { useForm } from 'react-hook-form'
-import { useRouter } from 'next/navigation'
-import { useCreateUserMutation } from '@/libs/api/users'
-import { useState } from 'react'
+import RegisterForm from './_components/RegisterForm'
+import RegisterPoster from './_components/Poster'
 export default function Register() {
-  const { register, handleSubmit } = useForm()
-  const router = useRouter()
-  const [error, setError] = useState(false)
-  const [createUser, { isLoading }] = useCreateUserMutation()
-
-  const handleCreateUser = async data => {
-    try {
-      const response = await createUser(data).unwrap()
-      console.log('New user created:', response)
-      sessionStorage.setItem('isLoggedIn', true)
-      router.push(pages.Dashboard)
-      /*
-      since the api does not give anything back assuming the user creating is success
-      in this case redirecting the user to dashboard page
-      and saving a key in session to keep track of login status in this case just
-      storing a true or false state
-      otherwise might use JWT token for authentication.
-      */
-    } catch (error) {
-      setError(true)
-      console.error('Failed to create User:', error)
-    }
-  }
-  const onSubmit = data => {
-    handleCreateUser(data)
-  }
   return (
     <CardContainer>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Grid container rowSpacing={2}>
-          <Grid item xs={12}>
-            <Typography
-              align='center'
-              variant='h5'
-              color={'primary'}
-              gutterBottom
-            >
-              Lets get you started !
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              size='small'
-              {...register('fullname')}
-              label='Full Name'
-              variant='outlined'
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              size='small'
-              {...register('username')}
-              label='Username'
-              variant='outlined'
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              size='small'
-              {...register('password')}
-              label='Password'
-              type='password'
-              variant='outlined'
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              type='submit'
-              variant='contained'
-              color='primary'
-              fullWidth
-              disabled={isLoading}
-            >
-              {isLoading ? 'Loading' : 'Register'}
-            </Button>
-          </Grid>
+      <Grid container rowSpacing={2}>
+        <Grid item xs={12}>
+          <Typography
+            color='primary'
+            variant='overline'
+            align='center'
+            gutterBottom
+          >
+            Lets get you started
+          </Typography>
         </Grid>
-      </form>
-      <Divider flexItem>
-        <Typography align='center' variant='overline'>
-          OR
-        </Typography>
-      </Divider>
-      <Button
-        type='submit'
-        variant='outlined'
-        color='primary'
-        fullWidth
-        onClick={() => {
-          router.push(pages.LOGIN)
-        }}
-      >
-        Login
-      </Button>
-      <Snackbar
-        open={error}
-        autoHideDuration={5000}
-        onClose={() => setError(false)}
-      >
-        <Alert
-          onClose={() => setError(false)}
-          severity='error'
-          variant='filled'
-          sx={{ width: '100%' }}
-        >
-          Something went wrong please try again.
-        </Alert>
-      </Snackbar>
+        <Grid item xs={12} sm={6} lg={6}>
+          <RegisterForm />
+        </Grid>
+        <Grid item xs={0} sm={6} lg={6}>
+          <RegisterPoster />
+        </Grid>
+      </Grid>
     </CardContainer>
   )
 }
